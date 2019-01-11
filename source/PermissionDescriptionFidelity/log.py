aa847189124921fe8c615681b18f898310dac915
Huseyin <huseyinalecakir@gmail.com>
2019-01-11 11:28:23 +0300

    Logger and timer is implemented. Functions data transfer can be analyzed easily now. Cosie proximity and cosine loss functions is implemented. Dependency based semantic similarity is implemented. Code refactoring is done.

---
+++ source/PermissionDescriptionFidelity/log.py
@@ -0,0 +1,30 @@
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    disable_existing_loggers = True,
+    version = 1,
+    formatters = {
+        'f': {'format':
+              '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
+        },
+    handlers = {
+        'stream': {'class': 'logging.StreamHandler',
+              'formatter': 'f',
+              'level': logging.INFO},
+        'file': {
+            'level': logging.INFO,
+            'formatter': 'f',
+            'class': 'logging.FileHandler',
+            'filename': 'out.log',
+            'mode': 'w',
+            }
+        },
+    root = {
+        'handlers': ['file'],
+        'level': logging.INFO,
+        },
+)
+
+dictConfig(logging_config)
+logger = logging.getLogger('project')
