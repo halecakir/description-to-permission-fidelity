0df6c4410701c60c01cfe6dc7463bb4ddbe4b8ba
Huseyin <huseyinalecakir@gmail.com>
2019-03-25 13:17:45 +0300

    Downloaded app statistics

---
+++ source/PermissionDescriptionFidelity/scripts/raw_data_statistics.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import inspect
+import csv
+
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+
+
+def apps_with_given_permission(file_path, included_permission):
+    """TODO"""
+    number_of_apps = 0
+    all_permissions = set()
+    apps_with_given_permission = 0
+    with open(file_path) as stream:
+        reader = csv.reader(stream)
+        header = next(reader)
+        for row in reader:
+            title = row[0]
+            text = row[1]
+            permissions = row[2]
+            link = row[3]
+
+            app_perms = {perm for perm in permissions.split("%%")}
+            number_of_apps += 1
+            if included_permission in app_perms:
+                apps_with_given_permission += 1
+            for permission in app_perms:
+                all_permissions.add(permission)
+    return number_of_apps, len(all_permissions), apps_with_given_permission
+
+if __name__ == "__main__":
+    DIR_NAME = os.path.dirname(__file__)
+    IN_PATH = os.path.join(DIR_NAME, "../../../data/big_processed/apps_processed.csv")
+    APP_COUNT, PERMISSION_COUNT, COUNT_APP_WITH_GIVEN_PERM = apps_with_given_permission(IN_PATH, "READ_CALENDAR")
+    print("Total Applications {}\n \
+          Total Distinct Permissions {}\n \
+          Application with given permisssion {}\n".format(APP_COUNT, PERMISSION_COUNT, COUNT_APP_WITH_GIVEN_PERM))
