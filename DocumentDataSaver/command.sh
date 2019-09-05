5c7de7245eb9478261000df967b21e6448a41779
Huseyin Alecakir <huseyinalecakir@gmail.com>
2019-09-05 19:19:12 +0300

    Unecessary arguments removed

---
+++ DocumentDataSaver/command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PARAMETERS_DIR="/home/huseyinalecakir/Security/data/saved-parameters"
+
+python runner.py --saved-data $PARAMETERS_DIR/saved-data/emdeddings-sentences-w2i.pickle 
+
+
+
