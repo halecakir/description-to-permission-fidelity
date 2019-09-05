d99212e5f2abf89b84f4a65fd2540a48750875f8
Huseyin Alecakir <huseyinalecakir@gmail.com>
2019-09-05 19:09:48 +0300

    DocumentDataSaver implemented for document based training

---
+++ DocumentDataSaver/common.py
@@ -0,0 +1,86 @@
+import torch
+import pandas as pd
+
+from utils.nlp_utils import NLPUtils
+
+class DocumentReport:
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.permissions = {}
+        self.preprocessed_sentences = []
+        self.sentences = []
+        self.prediction_result = None
+        self.index_tensors = None
+        
+class SentenceReport:
+    def __init__(self, id, sentence):
+        self.app_id = id
+        self.sentence = sentence
+        self.permissions = {}
+        self.preprocessed_sentence = None
+        self.prediction_result = None
+        self.index_tensor = None
+
+class Review:
+    def __init__(self, sentence, score):
+        self.sentence = sentence
+        self.preprocessed_sentence = None
+        self.score = score
+        self.index_tensor = None
+        self.prediction_result = None
+
+        
+def __load_row_document_acnet_file(infile, stemmer, embeddings):
+    print("Loading row {} ".format(infile))
+    #read training data
+    print("Reading Train Sentences")
+    tagged_train_file = pd.read_csv(infile)
+    documents = []
+    acnet_map = {
+        "RECORD_AUDIO": "MICROPHONE",
+        "READ_CONTACTS": "CONTACTS",
+        "READ_CALENDAR": "CALENDAR",
+        "ACCESS_FINE_LOCATION": "LOCATION",
+        "CAMERA": "CAMERA",
+        "READ_SMS": "SMS",
+        "READ_CALL_LOGS": "CALL_LOG",
+        "CALL_PHONE": "PHONE",
+        "WRITE_SETTINGS": "SETTINGS",
+        "GET_TASKS": "TASKS",
+    }
+         
+    for idx, row in tagged_train_file.iterrows():
+        app_id = int(row["app_id"])
+        sentence = row["sentence"]
+        
+        if documents == []: #if it is the first document
+            documents.append(DocumentReport(app_id))
+        elif documents[-1].app_id != app_id: # if it is a new document
+            documents.append(DocumentReport(app_id))
+            
+        for permission in acnet_map:
+            documents[-1].permissions[permission] = row[acnet_map[permission]]
+            
+        documents[-1].sentences.append(sentence)
+        preprocessed = NLPUtils.preprocess_sentence(sentence, stemmer)
+        documents[-1].preprocessed_sentences.append([word for word in preprocessed if word in embeddings])
+
+    print("Loading completed")
+    return documents
+
+
+def create_document_index_tensors(documents, w2i):
+    def get_tensor(sequence, w2i):
+        index_tensor = torch.zeros((1, len(sequence)), dtype=torch.long)
+        for idx, word in enumerate(sequence):
+            index_tensor[0][idx] = w2i[word]
+        return index_tensor
+
+    for document in documents:
+        document.index_tensors = [] 
+        for preprocessed_sentence in document.preprocessed_sentences:
+            index_tensor = get_tensor(preprocessed_sentence, w2i)
+            document.index_tensors.append(index_tensor)
+
+            
+            
---
+++ DocumentDataSaver/document_dump.py
@@ -0,0 +1,57 @@
+import os
+import pickle
+
+import common
+
+seed = 10
+
+class Data:
+    def __init__(self):
+        self.w2i = None
+        self.entries = None
+        self.train_entries = None
+        self.test_entries = None
+        self.ext_embedding = None
+        self.reviews = None
+        self.predicted_reviews = None
+
+    def load(self, infile):
+        with open(infile, "rb") as target:
+            self.ext_embeddings, self.entries, self.w2i = pickle.load(target)
+    
+    def save(self, outfile, list_of_objects):
+        with open(outfile, "wb") as target:
+            pickle.dump([self.ext_embeddings, self.entries, self.w2i], target)
+        
+    def load_predicted_reviews(self, infile):
+        with open(infile, "rb") as target:
+            self.predicted_reviews = pickle.load(target)
+        for app_id in self.predicted_reviews.keys():
+            self.predicted_reviews[app_id].sort(
+                key=lambda x: x.prediction_result.item(), reverse=True
+            )
+            
+    def load_reviews(self, infile):
+        with open(infile, "rb") as target:
+            self.reviews = pickle.load(target)
+            
+
+def run(args):
+    opt = TorchOptions()
+    
+    data = Data()
+    data.load(args.saved_data)
+    
+    DATASET_DIR = os.environ['DATASETS']
+    AC_NET_IN = os.path.join(DATASET_DIR, "acnet-data/ACNET_DATASET.csv")
+    
+    documents = common.__load_row_document_acnet_file(AC_NET_IN, args.stemmer, data.ext_embeddings)
+    common.create_document_index_tensors(documents, data.w2i)
+    
+    #replace sentence entries with documents
+    data.entries = documents
+    args.saved_data = args.saved_data.replace("sentences", "documents")
+    data.save(args.saved_data)
+    
+    
+    
