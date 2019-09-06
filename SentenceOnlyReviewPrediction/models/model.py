6e75bae86378883563276a26ea8d650b27dae02e
Huseyin Alecakir <huseyinalecakir@gmail.com>
2019-09-06 11:59:41 +0300

    Model save path is corrected

--- SentenceOnlyReviewPrediction/models/model.py
+++ SentenceOnlyReviewPrediction/models/model.py
@@ -318,7 +318,7 @@ def run(args):
 
     # Load model
     model = Model()
-    model.load(args.checkpoint)
+    model.load(args.model_checkpoint)
     # Test all reviews
     test_all_reviews(model, data.reviews)
     # Save reviews with predictions
