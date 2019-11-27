cc461d01cd26288fe0609bb220422a03d0a99f4d
Huseyin Alecakir <huseyinalecakir@gmail.com>
2019-11-27 14:54:20 +0300

    Run Scipts Modified

--- SentenceOnlyAttentionDynet/command.sh
+++ SentenceOnlyAttentionDynet/command.sh
@@ -6,8 +6,8 @@ ENCODER_TYPE=$3
 
 EXPERIMENT_TYPE=$ENCODER_DIR-$ENCODER_TYPE
 
-MODEL_TYPE="SentenceOnlyAttentionDyNET_$EXPERIMENT_TYPE"
-OUTPUT_DIR="../output/$MODEL_TYPE"
+MODEL_TYPE="SentenceOnlyAttentionDyNET"
+OUTPUT_DIR="../output/$MODEL_TYPE/$EXPERIMENT_TYPE"
 PARAMETERS_DIR="/home/huseyinalecakir/Security/data/saved-parameters"
 
 # Create output directory if not exists
@@ -19,7 +19,7 @@ python runner.py 	--permission-type $PERMISSION_TYPE \
 					--saved-data $PARAMETERS_DIR/saved-data/emdeddings-sentences-w2i.pickle \
 					--saved-reviews $PARAMETERS_DIR/saved-data/reviews.pickle \
 					--saved-predicted-reviews $PARAMETERS_DIR/saved-data/predicted-$PERMISSION_TYPE-reviews.pickle \
-					--model-checkpoint $PARAMETERS_DIR/saved-models/$MODEL_TYPE-$PERMISSION_TYPE.pt \
+					--model-checkpoint $PARAMETERS_DIR/saved-models/$MODEL_TYPE-$EXPERIMENT_TYPE-$PERMISSION_TYPE.pt \
 					--outdir $OUT_FILE \
 					--encoder-dir $ENCODER_DIR \
 					--encoder-type $ENCODER_TYPE
