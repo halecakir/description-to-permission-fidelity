e48eaf7775a94f75e52e6f154f4a33aa67c2ebb1
Huseyin Alecakir <huseyinalecakir@gmail.com>
2020-01-19 22:56:18 +0300

    Paper analysis and DataSaver are added.

---
+++ DataSaver/common.py
@@ -0,0 +1,26 @@
+class SentenceReport:
+    def __init__(self, id, sentence):
+        self.app_id = id
+        self.sentence = sentence
+        self.permissions = {}
+        self.preprocessed_sentence = None
+        self.prediction_result = None
+        self.index_tensor = None
+
+
+class Review:
+    def __init__(self, sentence, score):
+        self.sentence = sentence
+        self.preprocessed_sentence = None
+        self.score = score
+        self.index_tensor = None
+        self.prediction_result = None
+
+class DocumentReport:
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.permissions = {}
+        self.preprocessed_sentences = []
+        self.sentences = []
+        self.prediction_result = None
+        self.index_tensors = None
